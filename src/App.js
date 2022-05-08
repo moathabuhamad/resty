0dce5ee306ebb27eacdccdb4c3d91e067b809201
moathabuhamad <moathabuhamad@outlook.com>
2022-05-08 19:02:35 +0300

    lab 27

--- src/App.js
+++ src/App.js
@@ -3,23 +3,40 @@ import React from 'react';
 import './style/base.css';
 import './style/header.css';
 import './style/form.css';
+import './style/results.css';
 import './style/footer.css';
-
 
 import Header from './components/header';
 import Form from './components/form';
+import Results from './components/results';
 import Footer from './components/footer';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      requestData: null,
+      resultsIn: null,
+    };
+  }
+
+  getResults = (requestData) => {
+    this.setState({ requestData, resultsIn: 'results' });
+  };
+
+  render = () => (
+    <div className='App'>
       <Header />
       <main>
-      <Form />
+        <Form handleInput={this.getResults} />
+        <Results
+          data={this.state.requestData}
+          resultsIn={this.state.resultsIn}
+        />
       </main>
       <Footer />
     </div>
   );
 }
 
-export default App; 
+export default App;
